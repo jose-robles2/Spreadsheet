e2f20775ecca0d46a67ebead643679727bbc2f60
jrobles <jrobles@eecs.wsu.edu>
2023-01-23 21:15:47 -0800

    added lambda functs

--- Solution/Homework-1/MyTree/Tree.cs
+++ Solution/Homework-1/MyTree/Tree.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,6 +33,7 @@ namespace Homework_1.MyTree
 
         /*
             Private insertNode. Recursive function that traverses the tree to insert the newData in the according spot. 
+            Utilizes lambda functions to traverse both left and right subtrees of the tree.
             Go left if newData is less than current node's data
             Go right if newData is less than current node's data
             Insert once left/right node is null
@@ -39,6 +41,27 @@ namespace Homework_1.MyTree
          */
         private bool insertNode(Node tree, int newData)
         {
+            // Lambda helper functions to traverse left and right
+            Func<Node, int, bool>traverseLeft = (node, newData) => {
+                if (tree.Left == null)
+                {
+                    tree.Left = new Node(newData);
+                    return true;
+                }
+                insertNode(tree.Left, newData);
+                return false; 
+            };
+            Func<Node, int, bool> traverseRight = (node, newData) =>
+            {
+                if (tree.Right == null)
+                {
+                    tree.Right = new Node(newData);
+                    return true;
+                }
+                insertNode(tree.Right, newData);
+                return false;
+            };
+
             if (tree == null)
             {
                 this.root = new Node(newData); 
@@ -47,21 +70,11 @@ namespace Homework_1.MyTree
             {
                 if (newData < tree.Data)
                 {
-                    if (tree.Left == null)
-                    {
-                        tree.Left = new Node(newData);
-                        return true;
-                    }
-                    insertNode(tree.Left, newData);
+                    return traverseLeft(tree.Left, newData);
                 }
                 else if (newData > tree.Data)
                 {
-                    if (tree.Right == null)
-                    {
-                        tree.Right = new Node(newData);
-                        return true;
-                    }
-                    insertNode(tree.Right, newData);
+                    return traverseRight(tree.Right, newData);  
                 }
                 else
                 {
