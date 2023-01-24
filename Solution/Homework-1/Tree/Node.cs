c79b7a8635c974e9ac8aaab8b24a700e95e2762a
jrobles <jrobles@eecs.wsu.edu>
2023-01-23 19:55:39 -0800

    Added skelton for tree and node classes

--- Solution/Homework-1/Tree/Node.cs
+++ Solution/Homework-1/Tree/Node.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Homework_1.Tree
 {
+    /*
+        Node class object. These make up the tree. An integer datatype is stored as the attribute for each node, as well
+        as left and right children objects. Both are Node objects so it is recursive. 
+     */
     internal class Node
     {
+        private int data;
+        private Node? left; 
+        private Node? right;
+
+        public int Data { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        Node (int data = 0)
+        {
+            Data = data;
+        }
     }
 }
--- Solution/Homework-1/Tree/Tree.cs
+++ Solution/Homework-1/Tree/Tree.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Homework_1.Tree
 {
+    /*
+        Tree class. Made up of Node class objects. Has a single data member named root which points
+        to the root, or the beginning of the tree. 
+     */
     internal class Tree
     {
+        private Node? root;
+
+        public Tree()
+        {
+            root = null;
+        }
     }
 }
