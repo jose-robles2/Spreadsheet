de410d1b2d46ce3089156aa45f7dc9f208553e7d
jrobles <jrobles@eecs.wsu.edu>
2023-02-11 22:35:38 -0800

    Added the rest of Cell.cs class

--- Solution/SpreadsheetEngine/Cell.cs
+++ Solution/SpreadsheetEngine/Cell.cs
@@ -8,14 +8,16 @@ namespace SpreadsheetEngine
 {
     /// <summary>
     /// Abstract base class that represents a Cell object in the spreadsheet.
+    /// Implements the INotifyPropertyChanged class since this class follows
+    /// the observer/subscriber pattern.
     /// </summary>
-    public abstract class Cell
+    public abstract class Cell : INotifyPropertyChanged
     {
         /// <summary>
         /// Index for a row.
         /// </summary>
         protected int rowIndex;
-        
+
         /// <summary>
         /// Index for a column.
         /// </summary>
@@ -27,16 +29,23 @@ namespace SpreadsheetEngine
         protected string text;
 
         /// <summary>
+        /// Represents the evaluated value fo the cell since cells can contain formulas.
+        /// </summary>
+        protected string value;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="Cell"/> class.
         /// </summary>
         /// <param name="newRowIndex"> Index of the row. </param>
         /// <param name="newColumnIndex"> Index of the column. </param>
         /// <param name="newText"> Text content. </param>
-        public Cell(int newRowIndex, int newColumnIndex, string newText)
+        /// <param name="newValue"> Cell's value content. </param>
+        public Cell(int newRowIndex = 0, int newColumnIndex = 0, string newText = "", string newValue = "")
         {
             this.rowIndex = newRowIndex;
             this.columnIndex = newColumnIndex;
             this.text = newText;
+            this.value = newValue;
         }
 
         /// <summary>
@@ -62,7 +71,8 @@ namespace SpreadsheetEngine
         }
 
         /// <summary>
-        /// Gets or sets the text for the cell.
+        /// Gets or sets the text for the cell. If new text is being set, notify all
+        /// delegates subscribed to this observer about a change in property "Text".
         /// </summary>
         public string Text
         {
@@ -77,7 +87,18 @@ namespace SpreadsheetEngine
                 {
                     this.text = value;
                 }
+
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Text"));
             }
+        }
+
+        /// <summary>
+        /// Gets the value field. Set can only be done internally within this namespace.
+        /// </summary>
+        public string Value
+        {
+            get { return this.value; }
+            internal set { this.value = value; }
         }
     }
 }
