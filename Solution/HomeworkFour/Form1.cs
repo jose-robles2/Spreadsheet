a8226c5764f31d5976235e0516ac4a968205bc63
jrobles <jrobles@eecs.wsu.edu>
2023-02-11 19:55:12 -0800

    Added proj, test proj, and stylecop

---
+++ Solution/HomeworkFour/Form1.cs
@@ -0,0 +1,20 @@
+// <copyright file="Form1.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+namespace HomeworkFour
+{
+    /// <summary>
+    /// MyForm class that points to the running instance of the form for this program.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
