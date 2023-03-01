4dd7273152e1e29dc26d961ceb5c56896fc26ad3
jrobles <jrobles@eecs.wsu.edu>
2023-02-28 17:57:32 -0800

    Added skeleton ExpressionTree.cs

--- Solution/SpreadsheetEngine/Expressions/ExpressionTree.cs
+++ Solution/SpreadsheetEngine/Expressions/ExpressionTree.cs
@@ -15,5 +15,38 @@ namespace SpreadsheetEngine.Expressions
     /// </summary>
     public class ExpressionTree
     {
+        /// <summary>
+        /// Math expression.
+        /// </summary>
+        private string expression;
+
+        // private node root = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionTree"/> class.
+        /// </summary>
+        /// <param name="expression"> Math expression. </param>
+        public ExpressionTree(string expression)
+        {
+            this.expression = expression;
+        }
+
+        /// <summary>
+        /// Set a variable with a value.
+        /// </summary>
+        /// <param name="varName"> Name of variable. </param>
+        /// <param name="varValue"> Value of variable. </param>
+        public void SetVariable(string varName, double varValue)
+        {
+        }
+
+        /// <summary>
+        /// Evaluate the expression.
+        /// </summary>
+        /// <returns> Result of expression. </returns>
+        public double Evaluate()
+        {
+            return 0;
+        }
     }
 }
