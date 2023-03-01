1f1f8a45c0d43a54484ee521ebabe215dd27da6a
jrobles <jrobles@eecs.wsu.edu>
2023-02-28 18:02:54 -0800

    Added menu skeleton and stylecop

---
+++ Solution/ExpressionApp/Menu.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Menu.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionApp
+{
+    /// <summary>
+    /// Internal class handling the menu.
+    /// </summary>
+    internal class Menu
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Menu"/> class.
+        /// </summary>
+        public Menu()
+        {
+            this.RunApp();
+        }
+
+        /// <summary>
+        /// Contains the main loop for the console app.
+        /// </summary>
+        public void RunApp()
+        {
+        }
+
+        /// <summary>
+        /// Display the menu.
+        /// </summary>
+        public void RenderMenu()
+        {
+        }
+    }
+}
