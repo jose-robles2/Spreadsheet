2e018140b40bb0cf9fff8f31c8bf95bb657f2ebb
jrobles <jrobles@eecs.wsu.edu>
2023-03-06 00:26:55 -0800

    added expressionTokenize() tests

--- Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/ExpressionTree/TokenizeExpressionTests.cs
+++ Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/ExpressionTree/TokenizeExpressionTests.cs
@@ -5,20 +5,26 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+using SpreadsheetEngine.Expressions;
+
 namespace Tests.SpreadsheetEngineTests.ExpressionsTests.ExpressionTree
 {
     internal class TokenizeExpressionTests
     {
-        private TokenizeExpressionTests classObject = new();
-
-        private System.Reflection.MethodInfo GetMethod(string methodName)
+        /// <summary>
+        /// Helper method for reflection. Retrives information of a private method in order to invoke it.
+        /// </summary>
+        /// <param name="classObject"> Object that has the private method. </param>
+        /// <param name="methodName"> Target name. </param>
+        /// <returns></returns>
+        private System.Reflection.MethodInfo GetMethod(SpreadsheetEngine.Expressions.ExpressionTree classObject, string methodName)
         {
             if (string.IsNullOrWhiteSpace(methodName))
             {
                 Assert.Fail("methodName cannot be null or whitespace");
             }
 
-            var method = this.classObject.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            var method = classObject.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
 
             if (method == null)
             {
@@ -30,30 +36,53 @@ namespace Tests.SpreadsheetEngineTests.ExpressionsTests.ExpressionTree
 
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under normal conditions.
+        /// Test for private method ExpressionTree.TokenizeExpression() under normal conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestNormal()
+        public void TokenizeExpressionTestNormal()
         {
-            MethodInfo methodInfo = this.GetMethod("TokenizeExpression");
+            SpreadsheetEngine.Expressions.ExpressionTree classObject = new();
+            string input = "A1+B1+C1+D1+E1";
+            List<string> expectedOutput = new List<string> { "A1", "+", "B1", "+", "C1", "D1", "+", "E1"};
 
-            Assert.AreEqual
+            MethodInfo methodInfo = this.GetMethod(classObject, "TokenizeExpression");
+
+            List<string>? actualOutput = (List<string>?)methodInfo.Invoke(classObject, new object[] { input });
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under edge conditions.
+        /// Test for private method ExpressionTree.TokenizeExpression() under edge conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestEdge()
+        public void TokenizeExpressionTestEdge()
         {
+            SpreadsheetEngine.Expressions.ExpressionTree classObject = new();
+            string input = "A1+B1+C1";
+            List<string> expectedOutput = new List<string> { "A1", "+", "B1", "+", "C1" };
+
+            MethodInfo methodInfo = this.GetMethod(classObject, "TokenizeExpression");
+
+            List<string>? actualOutput = (List<string>?)methodInfo.Invoke(classObject, new object[] { input });
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under exception conditions.
+        /// Test for private method ExpressionTree.TokenizeExpression() under exception conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestException()
+        public void TokenizeExpressionTestException()
         {
+            SpreadsheetEngine.Expressions.ExpressionTree classObject = new();
+            string input = "A1";
+
+            MethodInfo methodInfo = this.GetMethod(classObject, "TokenizeExpression");
+
+            List<string>? actualOutput = (List<string>?)methodInfo.Invoke(classObject, new object[] { input });
+
+            Assert.Throws<ArgumentException>(() => methodInfo.Invoke(classObject, new object[] { input }));
         }
     }
 }
