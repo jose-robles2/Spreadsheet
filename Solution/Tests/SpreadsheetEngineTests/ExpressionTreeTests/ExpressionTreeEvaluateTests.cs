4280f8f6593621a47dce3a241162968e22c41546
jrobles <jrobles@eecs.wsu.edu>
2023-03-05 19:25:04 -0800

    Added skeleton classes for Evaluate() tests

---
+++ Solution/Tests/SpreadsheetEngineTests/ExpressionTreeTests/ExpressionTreeEvaluateTests.cs
@@ -0,0 +1,48 @@
+﻿using SpreadsheetEngine.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.SpreadsheetEngineTests.ExpressionTreeTests
+{
+    /// <summary>
+    /// Tests for ExpressionTree.Evaluate()
+    /// </summary>
+    internal class ExpressionTreeEvaluateTests
+    {
+        /// <summary>
+        /// Setup function used to setup different objects needed for testing.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test for ExpressionTree.Evaluate() under normal conditions.
+        /// </summary>
+        [Test]
+        public void EvaluateTestNormal()
+        {
+        }
+
+        /// <summary>
+        /// Test for ExpressionTree.Evaluate() under edge conditions.
+        /// </summary>
+        [Test]
+        public void EvaluateTestEdge()
+        {
+        }
+
+        /// <summary>
+        /// Test for ExpressionTree.Evaluate() under exception conditions.
+        /// </summary>
+        [Test]
+        public void EvaluateTestException()
+        {
+        }
+    }
+}
