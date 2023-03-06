ba3e1d19b61193f4a13172ce99ca396b21566ee1
jrobles <jrobles@eecs.wsu.edu>
2023-03-06 00:56:32 -0800

    Added SetVariable() tests

--- Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/ExpressionTree/SetVariableTests.cs
+++ Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/ExpressionTree/SetVariableTests.cs
@@ -9,40 +9,51 @@ using System.Threading.Tasks;
 namespace Tests.SpreadsheetEngineTests.ExpressionTreeTests.ExpressionTree
 {
     /// <summary>
-    /// Tests for ExpressionTree.Evaluate()
+    /// Tests for ExpressionTree.SetVariable(string varName, double varValue)
     /// </summary>
     internal class ExpressionTreeEvaluateTests
     {
+        /// <summary>
+        /// Tree for testing purposes.
+        /// </summary>
+        private SpreadsheetEngine.Expressions.ExpressionTree tree;
+        
         /// <summary>
         /// Setup function used to setup different objects needed for testing.
         /// </summary>
         [SetUp]
         public void Setup()
         {
+            tree = new SpreadsheetEngine.Expressions.ExpressionTree();
         }
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under normal conditions.
+        /// Test for ExpressionTree.SetVariable(string varName, double varValue) under normal conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestNormal()
+        public void SetVariableTestNormal()
         {
+            tree.SetVariable("A1", 2);
+            Assert.That(tree.GetVariable("A1"), Is.EqualTo(2));
         }
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under edge conditions.
+        /// Test for ExpressionTree.SetVariable(string varName, double varValue) under edge conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestEdge()
+        public void SetVariableTestEdge()
         {
+            tree.SetVariable("C1", int.MaxValue);
+            Assert.That(tree.GetVariable("C1"), Is.EqualTo(int.MaxValue));
         }
 
         /// <summary>
-        /// Test for ExpressionTree.Evaluate() under exception conditions.
+        /// Test for ExpressionTree.SetVariable(string varName, double varValue) under exception conditions.
         /// </summary>
         [Test]
-        public void EvaluateTestException()
+        public void SetVariableTestException()
         {
+            Assert.Throws<KeyNotFoundException>(() => tree.SetVariable("nonExistant", 100));
         }
     }
 }
