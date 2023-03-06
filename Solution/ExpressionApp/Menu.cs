f6cb7518557add90b05fb7accd477c80aa1a9631
jrobles <jrobles@eecs.wsu.edu>
2023-03-05 19:14:29 -0800

    Added some more logic for menu

--- Solution/ExpressionApp/Menu.cs
+++ Solution/ExpressionApp/Menu.cs
@@ -95,6 +95,14 @@ namespace ExpressionApp
         /// </summary>
         private void CreateExpressionOption()
         {
+            Console.WriteLine("Enter a new expression: ");
+            string? expression = Console.ReadLine();
+
+            if (expression != null)
+            {
+                this.expressionTree = new ExpressionTree(expression);
+                Console.WriteLine("New expression created");
+            }
         }
 
         /// <summary>
@@ -102,6 +110,15 @@ namespace ExpressionApp
         /// </summary>
         private void SetVariableOption()
         {
+            Console.WriteLine("Enter the variable name you want to set: ");
+            string? varName = Console.ReadLine();
+            Console.WriteLine("Enter the value you want to set to this variable: ");
+            string? varValue = Console.ReadLine();
+
+            if (varName != null && varValue != null)
+            {
+                this.expressionTree.SetVariable(varName, double.Parse(varValue));
+            }
         }
 
         /// <summary>
@@ -109,6 +126,7 @@ namespace ExpressionApp
         /// </summary>
         private void EvaluateTreeOption()
         {
+            Console.WriteLine("Evaluating Tree...\nResult: " + this.expressionTree.Evaluate());
         }
     }
 }
