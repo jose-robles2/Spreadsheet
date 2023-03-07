2376b0713fa8353a1337d3fb53744a8f9bc36254
jrobles <jrobles@eecs.wsu.edu>
2023-03-06 20:22:48 -0800

    Added Operators

---
+++ Solution/SpreadsheetEngine/Expressions/Operators/AddOperator.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="AddOperator.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine.Expressions.Operators
+{
+    /// <summary>
+    /// Implementation for AddOperator.
+    /// </summary>
+    public class AddOperator : Operator
+    {
+        /// <summary>
+        /// Method to be implemented in derived classes.
+        /// </summary>
+        /// <param name="left"> Value of left child. </param>
+        /// <param name="right"> Value of right child. </param>
+        /// <returns> double. </returns>
+        public override double Evaluate(double left, double right)
+        {
+            return left + right;
+        }
+    }
+}
---
+++ Solution/SpreadsheetEngine/Expressions/Operators/Operator.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Operator.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpreadsheetEngine.Expressions.Nodes;
+
+namespace SpreadsheetEngine.Expressions.Operators
+{
+    /// <summary>
+    /// Interface operator.
+    /// </summary>
+    public abstract class Operator
+    {
+        /// <summary>
+        /// Method to be implemented in derived classes.
+        /// </summary>
+        /// <param name="left"> Value of left child. </param>
+        /// <param name="right"> Value of right child. </param>
+        /// <returns> double. </returns>
+        public abstract double Evaluate(double left, double right);
+    }
+}
