b3b6f14dabd8fde7ceb57cc834e1694772887981
jrobles <jrobles@eecs.wsu.edu>
2023-03-08 19:23:35 -0800

    added overloaded builder for char ops

--- Solution/SpreadsheetEngine/Expressions/OperatorNodeFactory.cs
+++ Solution/SpreadsheetEngine/Expressions/OperatorNodeFactory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// <copyright file="OperatorNodeFactory.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -20,11 +24,39 @@ namespace SpreadsheetEngine.Expressions
             { DivOperator.OpString, new DivOperator() },
         };
 
+        /// <summary>
+        /// Static Builder method. Factory returns an Operator node that contains a
+        /// certain kind of Operator object.Inserts the left and right children as well.
+        /// </summary>
+        /// <param name="op"> string operator. </param>
+        /// <param name="left"> left child. </param>
+        /// <param name="right"> right child. </param>
+        /// <returns> new OperatorNode. </returns>
         public static Node Builder(string op, Node left, Node right)
         {
             if (SupportedOps.ContainsKey(op))
             {
                 return new OperatorNode(SupportedOps[op], left, right);
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Token {op} is not a supported operator");
+            }
+        }
+
+        /// <summary>
+        /// Static Builder method. Factory returns an Operator node that contains a
+        /// certain kind of Operator object.Inserts the left and right children as well.
+        /// </summary>
+        /// <param name="op"> char operator. </param>
+        /// <param name="left"> left child. </param>
+        /// <param name="right"> right child. </param>
+        /// <returns> new OperatorNode. </returns>
+        public static Node Builder(char op, Node left, Node right)
+        {
+            if (SupportedOps.ContainsKey(op.ToString()))
+            {
+                return new OperatorNode(SupportedOps[op.ToString()], left, right);
             }
             else
             {
