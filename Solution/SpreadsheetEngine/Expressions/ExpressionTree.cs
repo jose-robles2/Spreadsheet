bec6d4c40f52e986f9d93d069fe5168f5dacfa8a
jrobles <jrobles@eecs.wsu.edu>
2023-03-10 16:49:35 -0800

    moved methods from expressionTree to expression

--- Solution/SpreadsheetEngine/Expressions/ExpressionTree.cs
+++ Solution/SpreadsheetEngine/Expressions/ExpressionTree.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+using SpreadsheetEngine.Expressions;
 using SpreadsheetEngine.Expressions.Nodes;
-using SpreadsheetEngine.Expressions.Operators;
-using SpreadsheetEngine.Spreadsheet;
+using Expression = SpreadsheetEngine.Expressions.Expression;
 
 namespace SpreadsheetEngine.Expressions
 {
@@ -39,7 +39,7 @@ namespace SpreadsheetEngine.Expressions
         {
             this.expression = string.IsNullOrEmpty(expression) ? this.defaultExpression : expression;
             this.variableDictionary = new Dictionary<string, double>();
-            this.inFixExpressionTokens = this.TokenizeExpression(this.expression);
+            this.ParseExpressionAndInitVarsDict();
             this.CreateExpressionTree();
         }
 
@@ -99,103 +99,19 @@ namespace SpreadsheetEngine.Expressions
         }
 
         /// <summary>
-        /// Parse the expression string into tokens. The string is scanned from left to right and checked to see
-        /// if we are pointing at a alphabet char or a digit char so that it can be added to the appropriate list.
+        /// Parse the string expression into a list of tokens and init the vars dictionary.
         /// </summary>
-        /// <param name="expression"> Expression. </param>
-        /// <returns> List of strings. </returns>
-        private List<string> TokenizeExpression(string expression)
+        private void ParseExpressionAndInitVarsDict()
         {
-            expression = expression.Replace(" ", string.Empty);
+            this.inFixExpressionTokens = Expressions.Expression.TokenizeExpression(this.expression);
 
-            List<string> vars = new(), consts = new(), expressionTokens = new();
-
-            bool weSawAChar = false, weSawAConst = false;
-            int currentVarsPos = 0, currentConstPos = 0;
-
-            for (int i = 0; i < expression.Length; i++)
+            foreach (string token in this.inFixExpressionTokens)
             {
-                char currentChar = expression[i];
-                if (this.IsTokenAlphabetical(currentChar.ToString()))
+                if (Expressions.Expression.IsTokenAlphabetical(token))
                 {
-                    if (weSawAConst)
-                    {
-                        throw new ArgumentException("ERROR: Variables must start with a letter, not digit.");
-                    }
-
-                    if (weSawAChar)
-                    {
-                        vars[currentVarsPos] += currentChar;
-                    }
-                    else
-                    {
-                        vars.Add(currentChar.ToString());
-                        weSawAChar = true;
-                    }
-                }
-                else if (this.IsTokenADigit(currentChar.ToString()))
-                {
-                    if (weSawAChar)
-                    {
-                        vars[currentVarsPos] += currentChar;
-                    }
-                    else
-                    {
-                        if (weSawAConst)
-                        {
-                            consts[currentConstPos] += currentChar;
-                        }
-                        else
-                        {
-                            consts.Add(currentChar.ToString());
-                            weSawAConst = true;
-                        }
-                    }
-                }
-                else
-                {
-                    if (weSawAConst)
-                    {
-                        weSawAConst = false;
-                        expressionTokens.Add(consts[currentConstPos]);
-                        currentConstPos++;
-                    }
-                    else
-                    {
-                        weSawAChar = false;
-                        expressionTokens.Add(vars[currentVarsPos]);
-                        currentVarsPos++;
-                    }
-
-                    expressionTokens.Add(currentChar.ToString());
+                    this.variableDictionary.Add(token, 0);
                 }
             }
-
-            // We reached the end of an expression and need to add the last variable or constant
-            if (weSawAChar)
-            {
-                expressionTokens.Add(vars[currentVarsPos]);
-            }
-            else if (weSawAConst)
-            {
-                expressionTokens.Add(consts[currentConstPos]);
-            }
-
-            if (expressionTokens.Count < 3)
-            {
-                throw new ArgumentException("ERROR: Expression must have at least two operands and one operator.");
-            }
-
-            // Added check for NUnit testing purposes
-            if (this.variableDictionary.Count == 0)
-            {
-                foreach (string var in vars)
-                {
-                    this.variableDictionary.Add(var, 0);
-                }
-            }
-
-            return expressionTokens;
         }
 
         /// <summary>
@@ -206,20 +122,20 @@ namespace SpreadsheetEngine.Expressions
         {
             Stack<Node> nodeStack = new Stack<Node>();
 
-            List<string> postfixExpression = this.ConvertInfixToPostFix(this.inFixExpressionTokens);
+            List<string> postfixExpression = Expressions.Expression.ConvertInfixToPostFix(this.inFixExpressionTokens);
 
             foreach (string token in postfixExpression)
             {
-                if (this.IsTokenADigit(token))
+                if (Expressions.Expression.IsTokenADigit(token))
                 {
                     nodeStack.Push(new ConstantNode(double.Parse(token)));
                 }
-                else if (this.IsTokenAnOperator(token))
+                else if (Expressions.Expression.IsTokenAnOperator(token))
                 {
-                    Node right = nodeStack.Pop();
-                    Node left = nodeStack.Pop();
-
-                    nodeStack.Push(OperatorNodeFactory.Builder(token, left, right));
+                    OperatorNode opNode = OperatorNodeFactory.Builder(token);
+                    opNode.Right = nodeStack.Pop();
+                    opNode.Left = nodeStack.Pop();
+                    nodeStack.Push(opNode);
                 }
                 else
                 {
@@ -228,89 +144,6 @@ namespace SpreadsheetEngine.Expressions
             }
 
             this.root = nodeStack.Pop();
-        }
-
-        /// <summary>
-        /// Convert an infix expression to postfix. For HW5 assume that operators are the same
-        /// and no parentheses are included, so precedence and associativity aren't dealt with now.
-        /// Because of this, this is a simplified version of the shunting yard algorithm.
-        /// </summary>
-        /// <param name="expression"> Infix expression. </param>
-        /// <returns> List of strings. </returns>
-        private List<string> ConvertInfixToPostFix(List<string> expression)
-        {
-            if (expression.Count < 3)
-            {
-                throw new ArgumentException("ERROR: Expression of tokens must have at least three tokens");
-            }
-
-            Stack<string> opStack = new Stack<string>();
-            List<string> output = new List<string>();
-
-            foreach (string token in expression)
-            {
-                if (this.IsTokenADigit(token))
-                {
-                    output.Add(token);
-                }
-                else if (this.IsTokenAnOperator(token))
-                {
-                    if (output.Count >= 2)
-                    {
-                        if (opStack.Count > 0)
-                        {
-                            output.Add(opStack.Pop());
-                        }
-                    }
-
-                    opStack.Push(token);
-                }
-                else
-                {
-                    output.Add(token);
-                }
-            }
-
-            while (opStack.Count > 0)
-            {
-                output.Add(opStack.Pop());
-            }
-
-            return output;
-        }
-
-        /// <summary>
-        /// Checks to see if token is a supported operator.
-        /// </summary>
-        /// <param name="token"> token string. </param>
-        /// <returns> bool. </returns>
-        private bool IsTokenAnOperator(string token)
-        {
-            return OperatorNodeFactory.SupportedOps.ContainsKey(token) ? true : false;
-        }
-
-        /// <summary>
-        /// If a token starts with a digit, then it is a digit token.
-        /// </summary>
-        /// <param name="token"> token string. </param>
-        /// <returns> bool. </returns>
-        private bool IsTokenADigit(string token)
-        {
-            List<char> digits = Enumerable.Range('0', 10).Select(i => (char)i).ToList();
-
-            return digits.Contains(token[0]);
-        }
-
-        /// <summary>
-        /// If a token starts with a letter, then it is a variable token.
-        /// </summary>
-        /// <param name="token"> token string. </param>
-        /// <returns> bool. </returns>
-        private bool IsTokenAlphabetical(string token)
-        {
-            List<char> alphabet = Enumerable.Range('A', 26).Select(i => (char)i).ToList();
-
-            return alphabet.Contains(char.ToUpper(token[0]));
         }
     }
 }
