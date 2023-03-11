5cce077f99adb4cafee4b7abe9e6b595a6e16ec2
jrobles <jrobles@eecs.wsu.edu>
2023-03-10 16:38:01 -0800

    Added Builder() that takes in only a char, also changed dictionary

--- Solution/SpreadsheetEngine/Expressions/OperatorNodeFactory.cs
+++ Solution/SpreadsheetEngine/Expressions/OperatorNodeFactory.cs
@@ -16,12 +16,12 @@ namespace SpreadsheetEngine.Expressions
 {
     internal class OperatorNodeFactory
     {
-        public static readonly Dictionary<string, Operator> SupportedOps = new Dictionary<string, Operator>
+        public static readonly Dictionary<string, Func<Operator>> SupportedOps = new Dictionary<string, Func<Operator>>
         {
-            { AddOperator.OpString, new AddOperator() },
-            { SubOperator.OpString, new SubOperator() },
-            { MultOperator.OpString, new MultOperator() },
-            { DivOperator.OpString, new DivOperator() },
+            { AddOperator.OpString, () => new AddOperator() },
+            { SubOperator.OpString, () => new SubOperator() },
+            { MultOperator.OpString, () => new MultOperator() },
+            { DivOperator.OpString, () => new DivOperator() },
         };
 
         /// <summary>
@@ -32,11 +32,11 @@ namespace SpreadsheetEngine.Expressions
         /// <param name="left"> left child. </param>
         /// <param name="right"> right child. </param>
         /// <returns> new OperatorNode. </returns>
-        public static Node Builder(string op, Node left, Node right)
+        public static OperatorNode Builder(string op, Node left, Node right)
         {
             if (SupportedOps.ContainsKey(op))
             {
-                return new OperatorNode(SupportedOps[op], left, right);
+                return new OperatorNode(SupportedOps[op](), left, right);
             }
             else
             {
@@ -48,15 +48,13 @@ namespace SpreadsheetEngine.Expressions
         /// Static Builder method. Factory returns an Operator node that contains a
         /// certain kind of Operator object.Inserts the left and right children as well.
         /// </summary>
-        /// <param name="op"> char operator. </param>
-        /// <param name="left"> left child. </param>
-        /// <param name="right"> right child. </param>
+        /// <param name="op"> string operator. </param>
         /// <returns> new OperatorNode. </returns>
-        public static Node Builder(char op, Node left, Node right)
+        public static OperatorNode Builder(string op)
         {
-            if (SupportedOps.ContainsKey(op.ToString()))
+            if (SupportedOps.ContainsKey(op))
             {
-                return new OperatorNode(SupportedOps[op.ToString()], left, right);
+                return new OperatorNode(SupportedOps[op]());
             }
             else
             {
