2e9ff157d65b96fc3aa2dc69e4ce618c1e6a7d4c
jrobles <jrobles@eecs.wsu.edu>
2023-04-04 21:10:30 -0700

    changed form dimensions

--- Solution/SpreadsheetFrontend/Form1.Designer.cs
+++ Solution/SpreadsheetFrontend/Form1.Designer.cs
@@ -47,11 +47,11 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 47;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1022, 546);
+            this.dataGridView1.Size = new System.Drawing.Size(1022, 586);
             this.dataGridView1.TabIndex = 0;
             // 
             // demoButton
