8670ae3ee7cd9e533b0329663daedd22c72cadd3
jrobles <jrobles@eecs.wsu.edu>
2023-04-20 22:52:40 -0700

    added more exceptional tests for evaluate

--- Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/NodesTests/OperatorEvaluateTests.cs
+++ Solution/Tests/SpreadsheetEngineTests/ExpressionsTests/NodesTests/OperatorEvaluateTests.cs
@@ -41,7 +41,7 @@ namespace Tests.SpreadsheetEngineTests.ExpressionTreeTests.Nodes
         }
 
         /// <summary>
-        /// Test for OperatorNode.Evaluate() under exception conditions.
+        /// Test for OperatorNode.Evaluate() under exception conditions - div by zero
         /// </summary>
         [Test]
         public void EvaluateTestException()
@@ -50,7 +50,59 @@ namespace Tests.SpreadsheetEngineTests.ExpressionTreeTests.Nodes
             Node right = new ConstantNode(0);
             Node operatorNode = new OperatorNode(new DivOperator(), left, right);
 
-            Assert.Throws<DivideByZeroException>(() => operatorNode.Evaluate(new Dictionary<string, double>()));
+            Assert.That(operatorNode.Evaluate(new Dictionary<string, double>()), Is.EqualTo(double.MinValue));
+        }
+
+        /// <summary>
+        /// Test for OperatorNode.Evaluate() under exception conditions - multiplication overflow
+        /// </summary>
+        [Test]
+        public void EvaluateTestException2()
+        {
+            Node left = new ConstantNode(double.MaxValue);
+            Node right = new ConstantNode(2);
+            Node operatorNode = new OperatorNode(new MultOperator(), left, right);
+
+            Assert.That(operatorNode.Evaluate(new Dictionary<string, double>()), Is.EqualTo(double.MaxValue));
+        }
+
+        /// <summary>
+        /// Test for OperatorNode.Evaluate() under exception conditions - addition overflow
+        /// </summary>
+        [Test]
+        public void EvaluateTestException3()
+        {
+            Node left = new ConstantNode(double.MaxValue);
+            Node right = new ConstantNode(2);
+            Node operatorNode = new OperatorNode(new AddOperator(), left, right);
+
+            Assert.That(operatorNode.Evaluate(new Dictionary<string, double>()), Is.EqualTo(double.MaxValue));
+        }
+
+        /// <summary>
+        /// Test for OperatorNode.Evaluate() under exception conditions - subtraction infinity
+        /// </summary>
+        [Test]
+        public void EvaluateTestException4()
+        {
+            Node left = new ConstantNode(double.MinValue);
+            Node right = new ConstantNode(double.MaxValue);
+            Node operatorNode = new OperatorNode(new SubOperator(), left, right);
+
+            Assert.That(operatorNode.Evaluate(new Dictionary<string, double>()), Is.EqualTo(double.MaxValue));
+        }
+
+        /// <summary>
+        /// Test for OperatorNode.Evaluate() under exception conditions - subtraction overflow
+        /// </summary>
+        [Test]
+        public void EvaluateTestException5()
+        {
+            Node left = new ConstantNode(double.MaxValue);
+            Node right = new ConstantNode(-1.0);
+            Node operatorNode = new OperatorNode(new SubOperator(), left, right);
+
+            Assert.That(operatorNode.Evaluate(new Dictionary<string, double>()), Is.EqualTo(double.MaxValue));
         }
     }
 }
